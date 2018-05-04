c8966d387f6fb005e27b3df7a84e0d5be5c7fa78
Knoll <noll.kyle@gmail.com>
2018-05-04 17:26:17 -0500

    Add pagination

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'jquery-rails'
+gem 'will_paginate',           '3.1.0'
+gem 'bootstrap-will_paginate', '1.0.0'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+names = ["Big Pete's Salads",
+         "Susie's Sammich Shop",
+         "Slimmer Down",
+         "Baking Fussion",
+         "Odd Foods",
+         "The Island Vine",
+         "The Parlour",
+         "The Minty Bite",
+         "Burger Giant",
+         "The Carribean Lobster",
+         "Underwater Cafe"]
+
+descriptions = ["Pete makes big salads.",
+                "Susie Q works in the kitchen to make sammiches.",
+                "Award winning hot dogs.",
+                "Enjoy a fussion of gourmet pizza and ice cream cake.",
+                "Enjoy the oddest, most eccentric foods in town.",
+                "Great view, great service, great grapes.",
+                "A place to sit and relax.",
+                "Peppermint brownies, German Chocolate Fudge, and more!",
+                "Burgers too big to fit in your mouth.",
+                "Pretty good lobster, kind of like the real thing. Just try it.",
+                "Freshest fish in town! *Must be able to breathe underwater."]
+
+
+street_names = ["Marshall Street",
+                "Route 41",
+                "4th Street",
+                "Heather Lane",
+                "Euclid Avenue",
+                "Sunset Drive", 
+                "Old Creek Rd",
+                "Brown St",
+                "Fulton St",
+                "Heather Lane",
+                "Sandy Dr"]
+
+11.times do |i|
+  Place.create name: names[i], description: descriptions[i], address: "#{(rand*1000).to_i} #{street_names[i]}"
+end
