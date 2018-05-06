298a579bc4be123e0ab230b804d7e5e45d298a57
Knoll <noll.kyle@gmail.com>
2018-05-05 22:41:09 -0500

    Photo create controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,15 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!
 
   def create
+    @place = Photo.find(params[:place_id])
+    @place.photos.create(photo_params.merge(user: current_user))
+    redirect_to root_path
   end
 
+  private
+
+    def photo_params
+      params.require(:photo).permit(:caption)
+    end
 end
